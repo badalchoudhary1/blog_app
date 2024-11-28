53fb44a7826f7bf1f2795ab6eb4cd6e1c9262652
badalchoudhary1 <badalror10@gmail.com>
2024-11-28 12:50:12 +0530

    update manytomanyfield in db

--- dbapp/models.py
+++ dbapp/models.py
@@ -7,3 +7,18 @@ class Page(models.Model):
     page_name=models.CharField(max_length=70)
     page_cat=models.CharField(max_length=70)
     page_publish_date=models.DateField()
+
+class Post(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    post_title=models.CharField(max_length=70)
+    post_cat=models.CharField(max_length=70)
+    post_publish_date=models.DateField()
+
+
+class Song(models.Model):
+    user=models.ManyToManyField(User)
+    song_name=models.CharField(max_length=70)
+    song_duration=models.IntegerField()
+
+    def written_by(self):
+        return ",".join([str(p) for p in self.user.all()])
